cb83d6cda4656323b1d3a99d97b9f6ccde37b545
MattiasSp <mattias.spangmyr@gmail.com>
2024-10-14 16:54:41 +0200

    Allow localhost as link resource URL

--- client/src/views/sources/LinkResourceView.tsx
+++ client/src/views/sources/LinkResourceView.tsx
@@ -44,7 +44,8 @@ export default function LinkResourceView({ existingData = [], initialData, onSub
     const validateUrl = (url: string) => {
         const pattern = new RegExp('^(https?:\\/\\/)?' +
             '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
-            '((\\d{1,3}\\.){3}\\d{1,3}))' +
+            '((\\d{1,3}\\.){3}\\d{1,3})|' +
+            '(localhost))' +
             '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
             '(\\?[;&a-z\\d%_.~+=-]*)?' +
             '(\\#[-a-z\\d_]*)?$', 'i');
