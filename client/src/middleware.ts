c43cfc18f8d48b4365cb6479a981251ce2772181
Mattias Spångmyr <mattias.spangmyr@gmail.com>
2025-02-05 11:17:39 +0100

    Define session cookie name when getting token (#69)

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -5,9 +5,14 @@ export async function middleware(req: NextRequest) {
   const authEnabled = process.env.AUTH_ENABLED === "true";
   const publicPaths = ["/api/auth", "/login", "/signup"];
   const isPublicPath = publicPaths.some((path) => req.nextUrl.pathname.startsWith(path));
+  const sessionCookie = process.env.NEXTAUTH_URL?.startsWith("https://") ? "__Secure-next-auth.session-token" : "next-auth.session-token";
 
   if (authEnabled && !isPublicPath) {
-    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+    const token = await getToken({
+      req,
+      secret: process.env.NEXTAUTH_SECRET,
+      cookieName: sessionCookie
+    });
 
     if (!token || token.error === "RefreshAccessTokenError") {
       const url = req.nextUrl.clone();
